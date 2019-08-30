179fef7ad29277f1095dea38ec0ed31f8316917a
Pamela Torres <pamelatgtz@gmail.com>
2019-08-30 14:21:57 -0500

    Entrega para el primer spring 30 de agosto 2019

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,10 +1,11 @@
-const mdLinks = require('../');
+import {mdLinks} from '../src/md-links.js';
 
-
-describe('mdLinks', () => {
-
-  it('should...', () => {
-    console.log('FIX ME!');
+describe('mdLinks Verificación de Links', () => {
+  test('Retorna una promesa que resulva un Arreglo de Objetos que represneta un link, que tine las propiedades href,text y file', () => {
+     return mdLinks(absolutePath, {validate: true})
+     .then ((res) => {
+       expect(res).toEqual(validate);
+     })
   });
 
 });
